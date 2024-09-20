02dd32e71c8dae0e898147f47cdff30d0ae830fb
TrippSC2 <46848743+trippsc2@users.noreply.github.com>
2024-09-20 14:03:23 -0400

    Initial release.

---
+++ generate-docs.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+cd ./roles/cqi
+
+aar_doc . markdown
+
+cd ./roles/pvs_console
+
+aar_doc . markdown
+
+cd ./roles/pvs_server
+
+aar_doc . markdown
+
+cd ./roles/pvs_target
+
+aar_doc . markdown
+
+cd ./roles/win_server_vda
+
+aar_doc . markdown
---
+++ roles/pvs_server/molecule-tests-2311.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+set -e
+
+MOLECULE_BOX="w2022_cis" MOLECULE_CITRIX_PVS_VERSION="2311" molecule test
+MOLECULE_BOX="w2019_cis" MOLECULE_CITRIX_PVS_VERSION="2311" molecule test
