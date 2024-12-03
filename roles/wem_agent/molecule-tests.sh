bc56737f57f2a70ee2082c12225dc48ef586f85e
TrippSC2 <46848743+trippsc2@users.noreply.github.com>
2024-12-03 09:59:35 -0500

    Added Server 2025 and Windows 11 24H2 to testing where appropriate.

--- roles/wem_agent/molecule-tests.sh
+++ roles/wem_agent/molecule-tests.sh
@@ -4,5 +4,6 @@ set -e
 
 MOLECULE_BOX="w2022_cis" molecule test
 MOLECULE_BOX="w2019_cis" molecule test
+MOLECULE_BOX="w2025_base" molecule test
 MOLECULE_BOX="w11_23h2_base" molecule test
 MOLECULE_BOX="w10_22h2_base" molecule test
